884d7aee7bbe986d728748597fcf7ad106c5e651
e-psi-lon <theg41g@gmail.com>
2025-08-11 21:21:00 +0200

    refactor(constants): Use a private helper function instead of repeating similar code for constants

--- epsi_bot/utils/constants.py
+++ epsi_bot/utils/constants.py
@@ -12,6 +12,7 @@ __all__ = [
 	"EMBED_ERROR_NO_RESULTS_FOUND",
 	"EMBED_ERROR_VIDEO_TOO_LONG",
 	"EMBED_ERROR_NOT_BOT_OWNER",
+	"EMBED_ERROR_PLAYLIST_EXISTS",
 	"OWNER_ID",
 	"YOUTUBE_REGEX",
 	"GET_FILE_HTTP_URL",
@@ -20,30 +21,23 @@ __all__ = [
 ]
 
 OWNER_ID = 708006478807695450
-EMBED_ERROR_QUEUE_EMPTY = discord.Embed(title="Error", description="The queue is empty.",
-                                        color=discord.Color.dark_red())
-EMBED_ERROR_PLAYLIST_NAME_DOESNT_EXIST = discord.Embed(
-	title="Error",
-	description="A playlist with this name does not exist. Existing playlists:",
-	color=discord.Color.dark_red()
-)
-EMBED_ERROR_BOT_NOT_CONNECTED = discord.Embed(
-	title="Error",
-	description="Bot is not connected to a voice channel.",
-	color=discord.Color.dark_red()
-)
-EMBED_ERROR_BOT_NOT_PLAYING = discord.Embed(title="Error", description="Bot is not playing anything.",
-                                            color=discord.Color.dark_red())
-EMBED_ERROR_INDEX_TOO_HIGH = discord.Embed(title="Error", description="The index is too high.",
-                                           color=discord.Color.dark_red())
-EMBED_ERROR_NAME_TOO_LONG = discord.Embed(title="Error", description="The name is too long.",
-                                          color=discord.Color.dark_red())
-EMBED_ERROR_NO_RESULTS_FOUND = discord.Embed(title="Error", description="No results found.",
-                                             color=discord.Color.dark_red())
-EMBED_ERROR_VIDEO_TOO_LONG = discord.Embed(title="Error", description="The video is too long.",
-                                           color=discord.Color.dark_red())
-EMBED_ERROR_NOT_BOT_OWNER = discord.Embed(title="Error", description="You are not the owner of the bot.",
-                                          color=discord.Color.dark_red())
+
+
+def _embed_error(description: str) -> discord.Embed:
+	return discord.Embed(title="Error", description=description, color=discord.Color.dark_red())
+
+
+EMBED_ERROR_QUEUE_EMPTY = _embed_error("The queue is empty.")
+EMBED_ERROR_PLAYLIST_NAME_DOESNT_EXIST = _embed_error("A playlist with this name does not exist. Existing playlists:")
+EMBED_ERROR_BOT_NOT_CONNECTED = _embed_error("The ot is not connected to a voice channel")
+EMBED_ERROR_BOT_NOT_PLAYING = _embed_error("The bot is not playing anything.")
+EMBED_ERROR_INDEX_TOO_HIGH = _embed_error("The index is too high.")
+EMBED_ERROR_NAME_TOO_LONG = _embed_error("The name is too long.")
+EMBED_ERROR_NO_RESULTS_FOUND = _embed_error("No results found.")
+EMBED_ERROR_VIDEO_TOO_LONG = _embed_error("The video is too long.")
+EMBED_ERROR_NOT_BOT_OWNER = _embed_error("You are not the owner of the bot.")
+EMBED_ERROR_PLAYLIST_EXISTS = _embed_error("A playlist with this name already exists.")
+
 YOUTUBE_REGEX = re.compile(
 	r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/((watch\?v=)|(embed/)|(v/)|(.+\?v=))?([^&=%?]{11})')
 GET_FILE_HTTP_URL = re.compile(r'^https?://[^\s/$.?#]+\.[^\s/]+/.*?([^/]+\.[^/\s?#]+)(?:\?.*)?(?:#.*)?$')
