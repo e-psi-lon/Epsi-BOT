2bbce22ac6bddad624b75715037c0391b25660dd
e-psi-lon <theg41g@gmail.com>
2025-08-13 15:03:48 +0200

    refactor(main): Prepare for ASGI/prod server support

--- epsi_bot/__main__.py
+++ epsi_bot/__main__.py
@@ -10,26 +10,24 @@ from epsi_bot.panel import create_app
 from epsi_bot.utils.loggers import CustomFormatter, parse_args
 
 aiomultiprocess.set_start_method("fork")
-if os.environ.get("DOCKER_ENV", False):
-	load_dotenv(".env.docker")
-else:
+
+if not os.environ.get("DOCKER_ENV", False):
 	load_dotenv()
 
 start_time = datetime.datetime.now()
 
+app = create_app()
+app.set_start_time(start_time)
 
-def main() -> None:
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(CustomFormatter("Root"))
+log_level = getattr(logging, parse_args().log_level.upper(), logging.WARNING)
+handler.setLevel(log_level)
+logging.basicConfig(level=log_level, handlers=[handler])
+
+def main():
 	os.system("clear")
-	app = create_app()
-	app.set_start_time(start_time)
-	# Default logger
-	handler = logging.StreamHandler(sys.stdout)
-	handler.setFormatter(CustomFormatter("Root"))
-	log_level = getattr(logging, parse_args().log_level.upper(), logging.INFO)
-	handler.setLevel(log_level)
-	logging.basicConfig(level=log_level, handlers=[handler])
 	app.run(host="0.0.0.0", port=8080, use_reloader=False)
-
 
 if __name__ == "__main__":
 	main()
